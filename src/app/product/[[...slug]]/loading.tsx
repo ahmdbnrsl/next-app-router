4cac998ed9d487428ff3d1b89008b3781ee89452
abrazax56 <ahmadbenirusli@gmail.com>
2024-05-28 08:59:51 +0700

    loading state, error handling and middleware

--- src/app/product/[[...slug]]/loading.tsx
+++ src/app/product/[[...slug]]/loading.tsx
@@ -1,10 +1,16 @@
 export default function Loading() {
-    const data = Array(10).fill(0);
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <div className='grid grid-cols-auto-fit gap-5'>
-            {data.map(() => (
-                <div className='place-items-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-                    <a href='#'>
+            {data.map((it: any, ins: number) => (
+                <div
+                    key={ins}
+                    className='place-items-center w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'
+                >
+                    <a
+                        id={it}
+                        href='#'
+                    >
                         <div className='w-full p-8 rounded-t-lg object-cover h-96 bg-gray-600'></div>
                     </a>
                     <div className='px-5 pb-5'>
