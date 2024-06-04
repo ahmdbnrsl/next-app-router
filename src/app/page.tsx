6dba85351e584527f6ee61b02884343b342b398c
abrazax56 <ahmadbenirusli@gmail.com>
2024-06-04 08:39:46 +0700

    optimizing: SEO Metadata, sitemap, robots.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Home',
+    description: 'Aplikasi saya',
+    authors: [
+        {
+            name: 'Via Fitriana',
+            url: 'https://instagram.com/viiiaaaa_fy'
+        }
+    ],
+    icons: {
+        icon: '/icon.png'
+    },
+    openGraph: {
+        title: 'Beni X Via',
+        description: 'I Love You'
+    }
+};
+
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
